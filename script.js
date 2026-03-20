3167f5b19c62391eea76eb03be6ef017f23ad548
Test User <test@example.com>
2026-03-20 19:33:11 +0700

    chore: update telegram config and domain

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ const db = firebase.firestore();
 // ============================================
 // TELEGRAM CONFIG
 // ============================================
-const TELEGRAM_BOT_TOKEN = '8601457526:AAEDpglDCgTX_qBoRDWNddVXK4MR-IS4AwE';
-const TELEGRAM_CHAT_ID = '644667498';
+const TELEGRAM_BOT_TOKEN = '7292150917:AAEvL-C_1zX6VpGvK6ZfC27q7a75G9-f7i4';
+const TELEGRAM_CHAT_ID = '-5207532142';
 
 async function sendTelegramNotification(email, phone, totalCount) {
     const message =
